fc73ee9e42c1711453878b8587bb9563d0dd2ac0
rostek93 <kamilrostecki@o2.pl>
2015-04-13 01:30:25 +0200

    Zmiany

--- SuperMemo/mainwindow.cpp
+++ SuperMemo/mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_mainwindow.h"
 #include "QtSql/QtSql"
 #include <QMessageBox>
+#include "learnwindow.h"
 
 using namespace std;
 
@@ -60,5 +61,6 @@ void MainWindow::on_actionZamknij_triggered()
 
 void MainWindow::on_learnButton_clicked()
 {
-
+    learnwindow
+   // lw.show();
 }
---
+++ SuperMemo/ui_mainwindow.h
@@ -0,0 +1,104 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.4.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QListWidget>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionZamknij;
+    QWidget *centralWidget;
+    QPushButton *pushButton;
+    QLabel *label;
+    QListWidget *listWidget_sets;
+    QPushButton *learnButton;
+    QMenuBar *menuBar;
+    QMenu *menuMenu;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(476, 361);
+        actionZamknij = new QAction(MainWindow);
+        actionZamknij->setObjectName(QStringLiteral("actionZamknij"));
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setGeometry(QRect(210, 150, 75, 23));
+        label = new QLabel(centralWidget);
+        label->setObjectName(QStringLiteral("label"));
+        label->setGeometry(QRect(360, 120, 47, 13));
+        listWidget_sets = new QListWidget(centralWidget);
+        listWidget_sets->setObjectName(QStringLiteral("listWidget_sets"));
+        listWidget_sets->setGeometry(QRect(10, 20, 171, 192));
+        learnButton = new QPushButton(centralWidget);
+        learnButton->setObjectName(QStringLiteral("learnButton"));
+        learnButton->setGeometry(QRect(210, 30, 75, 23));
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 476, 21));
+        menuMenu = new QMenu(menuBar);
+        menuMenu->setObjectName(QStringLiteral("menuMenu"));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        menuBar->addAction(menuMenu->menuAction());
+        menuMenu->addAction(actionZamknij);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
+        actionZamknij->setText(QApplication::translate("MainWindow", "Zamknij", 0));
+        pushButton->setText(QApplication::translate("MainWindow", "Logowanie", 0));
+        label->setText(QApplication::translate("MainWindow", "TextLabel", 0));
+        learnButton->setText(QApplication::translate("MainWindow", "Nauka", 0));
+        menuMenu->setTitle(QApplication::translate("MainWindow", "Menu", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
